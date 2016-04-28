76781da2598d97d5edbcba01ddeffacf09c5166a
Rafał Suszka <suszkarafal@gmail.com>
2016-04-28 20:28:13 +0200

    lab5

--- lab5.0/lab5.0/Source.c
+++ lab5.0/lab5.0/Source.c
@@ -1,3 +1,17 @@
+/*Napisaæ program który wczytuje pliki binarne  do tablicy dynamicznej tab:
+Hex * tab;
+typedef struct HexByte
+ {
+   char [2]; // reprezntacja bajtu w postaci 16-owej (ascii)
+   unsigned char;// w postci binarnej
+ } HexByte
+ 
+Program wywo³uje siê z lini komend (cmd) z nastepuj¹cymi opcjami:
+hex.exe plik.in -ot plik.txt // zapisuje wczytany plik w postaci tekstowej
+hex.exe plik.in -ob plik.bin  // zapisuje wczytany plik w postaci binarne
+hex.exe plik.in  -d FA36  // usuwa z pliku ci¹g bajtów 
+hex.exe plik.in  -r  DB A908 FFAA33 // zastêpuje jeden ci¹g bajtów innym ci¹giem
+Opcje -d i -r mog¹ wystêpowaæ opcj¹ -ot albo -ob*/
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct HexByte_
@@ -62,7 +76,7 @@ void konw_na_16(int kod_ASCI, char* t_bajt)
 		kod_ASCI = kod_ASCI / 16;
 	}
 }
-void wczytaj_plik_b(HexByte* tab, char* nazwa_pliku)
+HexByte** wczytaj_plik_b(HexByte* tab, char* nazwa_pliku, int* dl_tab)
 {
 	FILE* plik;
 	int licznik = 1;
@@ -86,11 +100,33 @@ void wczytaj_plik_b(HexByte* tab, char* nazwa_pliku)
 		licznik++;  //ostatni element wczytuje siê 2x !!
 	}
 	fclose(plik);
+	*dl_tab = licznik - 1;
+	return tab;
+}
+void zapisz_txt(HexByte* tab, char* nazwa_pliku, int dlugosc_tab)
+{
+	FILE* plik;
+	HexByte bufor;
+	int licznik;
+	fopen_s(&plik, nazwa_pliku, "w");
+	if (plik == NULL)
+	{
+		printf("Blad otwarica pliku\n");
+		exit(-10);
+	}
+	for (int licznik = 0; licznik < dlugosc_tab; licznik++)
+	{
+		bufor = tab[licznik];
+		fwrite(&bufor.bajt_b, sizeof(char), 1, plik);
+	}
+	fclose(plik);
 }
 int main()
 {
 	HexByte* tablica = NULL;
-	wczytaj_plik_b(tablica, "abc");
+	int dl_tab;
+	tablica = wczytaj_plik_b(tablica, "abc", &dl_tab);
+	zapisz_txt(tablica, "abcd.txt", dl_tab);
 	system("Pause");
 	return 0;
 }
